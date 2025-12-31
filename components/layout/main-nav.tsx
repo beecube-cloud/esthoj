4f6057fdb3cc5f2ea8f1511b5eb28159fa33d249
tizah <davidzagi@gmail.com>
2025-12-31 21:38:19 +0100

    update consultancy link in nav bar

--- components/layout/main-nav.tsx
+++ components/layout/main-nav.tsx
@@ -20,8 +20,8 @@ export default function NavbarScrollWithLogo() {
   const navLinks = [
     { name: 'Home', href: '/' },
     { name: 'Construction', href: 'https://construction.esthoj.com', target: '_blank' }, 
-    { name: 'Consultancy', href: '/consultancy', target: '_blank' },
-    { name: 'Training', href: '/training', target: '_blank' },
+    { name: 'Consultancy', href: 'https://test-consultancy-ashy.vercel.app/', target: '_blank' },
+    // { name: 'Training', href: '/training', target: '_blank' },
   ];
 
   useEffect(() => {
